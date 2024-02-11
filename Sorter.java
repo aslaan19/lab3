91bfd9f81f69c026a780cd58ce2ffc44ebdd2e93
Aslan <abullahaslan3010@gmail.com>
2024-02-11 17:36:59 +0300

    push

--- Sorter.java
+++ Sorter.java
@@ -20,5 +20,20 @@ class lab03 {
 			}
 			array[j + 1] = key;
 		}
+
+	}
+	private static void selectionSort(int[] array) {
+		int len = array.length;
+		for (int i = 0; i < len - 1; i++) {
+
+			int smallest = i;
+			for (int j = i + 1; j < len; j++)
+				if (array[j] < array[smallest])
+					smallest = j;
+
+			int temp = array[smallest];
+			array[smallest] = array[i];
+			array[i] = temp;
+		}
 	}
 }
