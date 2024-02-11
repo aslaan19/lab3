8f186f62c28cd4c2c360f9e2a620660a0eed0126
SauceX <mahmoudzak4@gmail.com>
2024-02-11 17:33:20 +0300

    insertion sort method

--- Sorter.java
+++ Sorter.java
@@ -1,9 +1,24 @@
-/**
- * Sorter
- */
-public class Sorter {
+class lab03 {
 	public static void main(String[] args) {
-		System.out.println("wassup");
+		System.out.println("testing...");
 	}
 
+	public static void sort(int[] array) {
+		// selectionSort(array);
+	}
+
+	private static void insertionSort(int[] array) {
+		for (int i = 1; i < array.length; ++i) {
+			int key = array[i];
+			int j = i - 1;
+
+			// Move elements of array[0..i-1], that are greater than key,
+			// to one position ahead of their current position
+			while (j >= 0 && array[j] > key) {
+				array[j + 1] = array[j];
+				j = j - 1;
+			}
+			array[j + 1] = key;
+		}
+	}
 }
